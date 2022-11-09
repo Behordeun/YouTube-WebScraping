b0b4f7f4e91c3a96d164307a400f720315cea8ad
Muhammad Abiodun SULAIMAN <prince.behordeun@gmail.com>
2022-11-09 16:28:04 +0100

    Delete .history directory

--- .history/app_20221109130337.py
+++
@@ -1,6 +0,0 @@
-# %
-from apiclient.discovery import build #pip install google-api-python-client
-from apiclient.errors import HttpError #pip install google-api-python-client
-from oauth2client.tools import argparser #pip install oauth2client
-import pandas as pd #pip install pandas
-import matplotlib.pyplot as plt #pip install matplotlib
